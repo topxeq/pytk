a94c8906d346b1396e9c4343b188d7b206fcc7e8
TopXeQ <topxeq@163.com>
2020-02-15 16:41:42 +0800

    a 0.1

---
+++ pytk.py
@@ -0,0 +1,15 @@
+#_*_coding:utf-8_*_
+
+# 使用时直接将该文件放在同目录下，或者Python的sys.path中即可
+# 引用时使用from pytk import pytk即可
+
+class pytk:
+    """Toolkit for Python written by TopXeQ"""
+
+    pytkVersion = '0.99a'
+
+    @classmethod
+    def test(cls):
+        print('test')
+
+
