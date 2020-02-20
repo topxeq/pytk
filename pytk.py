9ac13c52c879677252ef39ab6373e3b62886674e
TopXeQ <topxeq@163.com>
2020-02-20 09:04:37 +0800

    a 0.1

--- pytk.py
+++ pytk.py
@@ -3,6 +3,9 @@
 # 使用时直接将该文件放在同目录下，或者Python的sys.path中即可
 # 引用时使用from pytk import pytk即可
 
+def outTest():
+    print('outTest')
+
 class pytk:
     """Toolkit for Python written by TopXeQ"""
 
@@ -12,4 +15,6 @@ class pytk:
     def test(cls):
         print('test')
 
+if __name__ == '__main__': 
+    pytk.test()
 
